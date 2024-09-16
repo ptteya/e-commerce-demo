69c38a48046e3fcd86afef5ec05601adf412c8ba
ptteya <pettyas@proton.me>
2024-09-16 17:33:46 +0300

    Fix AdminRoute logic

--- client/src/components/AdminRoute.js
+++ client/src/components/AdminRoute.js
@@ -1,5 +1,5 @@
-import { AuthContext } from "contexts/AuthContext"
-import { useContext } from "react"
+import { AuthContext } from "contexts/AuthContext";
+import { useContext } from "react";
 import { Navigate } from "react-router-dom";
 
 const AdminRoute = ({ children }) => {
@@ -7,7 +7,7 @@ const AdminRoute = ({ children }) => {
 
     if (isAuthenticated && user.role !== 'admin') {
         return <Navigate to='/' />
-    } else if (!isAuthenticated && user.role !== 'admin') {
+    } else if (!isAuthenticated) {
         return <Navigate to='/auth/login' />
     }
 
