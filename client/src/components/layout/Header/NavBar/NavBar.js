a547c882cf15999a90a1e61302b75717b46c5c66
ptteya <pettyas@proton.me>
2024-09-29 17:35:39 +0300

    Update NavBar; click to toggle catalog menu

--- client/src/components/layout/Header/NavBar/NavBar.js
+++ client/src/components/layout/Header/NavBar/NavBar.js
@@ -1,29 +1,32 @@
 import { Link } from "react-router-dom";
 import { categoryOptions } from "constants/categoryOptions";
 import { formatCategoryTitle } from "utils/formatCategoryTitle";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "contexts/AuthContext";
 import Search from "../Search/Search";
 import './NavBar.css';
 
 const NavBar = () => {
     const { user } = useContext(AuthContext);
+    const [isMenuOpen, setIsMenuOpen] = useState();
+
+    const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
 
     return (
-        <div className="nav-bar">
+        <div className="navbar">
             <div className="logo-container">
                 <div className="logo-image"></div>
                 <Link to="/" className="logo">HOMELY</Link>
             </div>
-            <ul className="nav-group">
+            <ul className="nav-list">
                 <li className="nav-item"><Link to="/">Home</Link></li>
-                <li className="nav-item catalog-link">
+                <li className="nav-item catalog-item" onClick={toggleMenu} >
                     <Link className='' onClick={(e) => e.preventDefault()}>
                         Catalog
-                        <i className="fas fa-chevron-down arrow-icon"></i>
+                        <i className={`fas fa-chevron-down arrow-icon ${isMenuOpen ? 'arrow-rotated' : ''}`}></i>
                     </Link>
 
-                    <ul className='dropdown-menu'>
+                    <ul onClick={toggleMenu} className={`dropdown-menu ${isMenuOpen ? 'active-menu' : ''}`} >
                         {categoryOptions.map(option => (
                             <li key={option}>
                                 <Link to={`/furniture?category=${option}`}>
