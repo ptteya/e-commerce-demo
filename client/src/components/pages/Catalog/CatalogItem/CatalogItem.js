a618aff058fa7710151bf19d78c38123228b4197
ptteya <pettyas@proton.me>
2024-10-03 20:52:51 +0300

    Refactor CatalogItem; add cart toggling

--- client/src/components/pages/Catalog/CatalogItem/CatalogItem.js
+++ client/src/components/pages/Catalog/CatalogItem/CatalogItem.js
@@ -1,35 +1,46 @@
 import './CatalogItem.css';
 import { useNavigate } from 'react-router-dom';
 import { useCollectionManager } from 'hooks/useCollectionManager';
+import { FaRegHeart, FaHeart } from "react-icons/fa";
+import { RiShoppingBagLine } from "react-icons/ri";
 
-const CatalogItem = ({
-    _id,
-    name,
-    price,
-    size,
-    images,
-}) => {
-    const { added, toggleCollectionItem } = useCollectionManager(_id, 'favorites');
+const CatalogItem = ({ _id, name, price, size, images }) => {
+    const { added: isFavorite, toggleCollectionItem: toggleFavorites } = useCollectionManager(_id, 'favorites');
+    const { added: isInCart, toggleCollectionItem: toggleCart } = useCollectionManager(_id, 'cart');
     const navigate = useNavigate();
 
     const handleCardClick = (e) => {
-        if (!e.target.classList.contains('heart-icon')) {
+        if (!e.target.closest('.action')) {
             navigate(`/furniture/${_id}`);
         }
     };
 
     return (
         <div className="product-card" onClick={handleCardClick}>
-            <div className="image-container">
+            <div className="card-image">
                 <img src={images.mainImage} alt="couch" />
-                <i className={`heart-icon ${added ? 'fas fa-heart favorites' : 'far fa-heart'}`} onClick={() => toggleCollectionItem()}></i>
+
+                <i
+                    className={`action heart-icon ${isFavorite ? 'favorite' : ''}`}
+                    onClick={() => toggleFavorites()}
+                >
+                    {isFavorite ? <FaHeart /> : <FaRegHeart />}
+                </i>
+
+                <div className='action add-cart' onClick={() => toggleCart()}>
+                    <div className="text">
+                        <RiShoppingBagLine className={`bag-icon ${isInCart ? 'in-cart' : ''}`} />
+                        <p>{isInCart ? "Remove from cart" : "Add to cart"}</p>
+                    </div>
+                </div>
             </div>
-            <div className="product-info">
+
+            <div className="product-info" >
                 <div className="container">
                     <h3>{name}</h3>
                 </div>
-                <p className="size"> Size: {size.width} x {size.height} x {size.length} cm</p>
-                <p className="price">${price}</p>
+                <p className="size">Size: {size.width} x {size.height} x {size.length} cm</p>
+                <p className="price">${price.toFixed(2)}</p>
             </div>
         </div>
     );
